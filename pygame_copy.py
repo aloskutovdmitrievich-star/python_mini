15998505e63216432ec1c03ed756bff85d6b9a06
aloskutovdmitrievich-star <a.loskutov.dmitrievich@gmail.com>
2026-03-06 17:59:47 +0300

    Delete pygame_copy.py

--- pygame_copy.py
+++
@@ -1,9 +0,0 @@
-import pygame
-class newproject():
-    pygame.init()
-    def __init__(self):
-        pass
-    def create_window(size, fill=pygame.Color("black"), name_window="pygame window"):
-        window = pygame.display.set_mode(size)
-        pygame.display.set_caption(name_window)
-        window.fill(fill)
